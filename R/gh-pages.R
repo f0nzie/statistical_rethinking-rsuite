2d3c04abb46bccee3bc290ce791b0802472fd91a
f0nzie <alfonso.reyes@oilgainsanalytics.com>
2019-06-18 17:23:24 -0500

    send to gh-pages.R

---
+++ R/gh-pages.R
@@ -0,0 +1,30 @@
+# Detect proper script_path (you cannot use args yet as they are build with tools in set_env.r)
+script_path <- (function() {
+  args <- commandArgs(trailingOnly = FALSE)
+  script_path <- dirname(sub("--file=", "", args[grep("--file=", args)]))
+  if (!length(script_path)) {
+    return("R")
+  }
+  if (grepl("darwin", R.version$os)) {
+    base <- gsub("~\\+~", " ", base) # on MacOS ~+~ in path denotes whitespace
+  }
+  return(normalizePath(script_path))
+})()
+
+# Setting .libPaths() to point to libs folder
+source(file.path(script_path, "set_env.R"), chdir = T)
+
+config <- load_config()
+args <- args_parser()
+
+###################################
+
+# git subtree push --prefix work/book/public https://github.com/f0nzie/statistical_rethinking-rsuite.git gh-pages
+
+git_cmd <- "git subtree push --prefix "
+output_folder <- "work/book/public"
+repository <- "https://github.com/f0nzie/statistical_rethinking-rsuite.git"
+gh_pages <- "gh-pages"
+
+send_cmd <- paste(git_cmd, output_folder, repository, gh_pages, sep = " ")
+system(send_cmd)
